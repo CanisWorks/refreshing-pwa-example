77760eae30626174902e4091e3912ef1644c21e6
Tom Wilderspin <tomwilderspin@gmail.com>
2023-01-27 13:57:49 +0000

    setting up versioned pwa example

---
+++ public/env-config/prod.config.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars */
+
+var REACT_APP_REGISTER_SW = true;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// extending the window to include the 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -17,9 +19,14 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+if (window.REACT_APP_REGISTER_SW === true) {
+  serviceWorkerRegistration.register();
+} else {
+  serviceWorkerRegistration.unregister();
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
