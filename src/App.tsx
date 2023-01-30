36c5b63253766ab74e754151449e39ab1d06f365
Tom Wilderspin <tomwilderspin@gmail.com>
2023-01-30 20:32:44 +0000

    updates app files with sw update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Routes, Route, Outlet, Link, useLocation } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  
+  const location = useLocation();
+  React.useEffect(() => {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.update();
+    });
+  }, [location]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +19,55 @@ function App() {
         <p>
           This is a PWA!
         </p>
+      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </div>
+      <footer>
         <p>
           Version: { process.env.REACT_APP_VERSION_ID || 'development' }
         </p>
-      </header>
+      </footer>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul className='App-nav'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 // extending the window to include the 
 
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -29,4 +32,4 @@ if (window.REACT_APP_REGISTER_SW === true) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
