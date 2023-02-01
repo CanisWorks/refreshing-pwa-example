79c6107be475ebf2ed40470c48d86242e3585837
Tom Wilderspin <tomwilderspin@gmail.com>
2023-02-01 17:21:08 +0000

    bump again

--- src/index.tsx
+++ src/index.tsx
@@ -24,9 +24,9 @@ const withSwRegistration = (WrappedComp: FC<WrappedCompProps>, ) => {
       if ('serviceWorker' in navigator) {
         navigator.serviceWorker.ready.then((registration) => {
           if (registration.waiting) {
-            // send the skip message to kick off the sw update.
+            // send the skip message to kick off the service worker install.
             registration.waiting.postMessage({type: 'SKIP_WAITING'});
-            // add an listener to reload page. 
+            // add an listener to reload page when the new service worker is ready.
             registration.waiting.addEventListener('statechange', (event: Event) => {
               const { state = '' } =  event.target as unknown as {state: string} || {};
               if (state === 'activated') {
